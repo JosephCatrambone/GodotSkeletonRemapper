4c2d0785e3f6f4197ef6a2eaa7d441afcbb02d4b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-07-15 17:33:43 -0700

    Use the global transform because of scaling issues.

--- VRMRemapper.gd
+++ VRMRemapper.gd
@@ -108,6 +108,7 @@ func make_features_for_skeleton(skeleton:Skeleton3D) -> Dictionary:
 	# Start by finding the depth of every bone.
 	for bone_id in skeleton.get_bone_count():
 		var pose:Transform3D = skeleton.get_bone_global_pose(bone_id)  # get_global_pose?
+		pose = skeleton.global_pose_to_world_transform(pose)
 		result[skeleton.get_bone_name(bone_id)] = [
 			# Position
 			pose.origin.x, pose.origin.y, pose.origin.z, 
